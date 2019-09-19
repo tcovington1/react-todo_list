4ea5372205f12b4710cb34f1dc62de1fb55ac83b
Taylor Covington <tcovingtondev@gmail.com>
2019-09-18 20:01:03 -0600

    created project and list compoment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import List from './components/todos/List'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//logical component
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, name: 'Learn Rails', complete: true },
+      { id: 2, name: 'Learn React', complete: false },
+      { id: 3, name: 'Learn React Router', complete: false },
+    ]
+  }
+
+  // renderTodos = () => {
+  //   const { todos } = this.state
+  //   //.map loops through the todos array
+  //   return todos.map( todo => {
+  //     return (
+  //       <li key={todo.id}>{todo.name}</li>
+  //       //key is a prop giving it an key value
+  //     )
+  //   })
+  // }
+
+  render () {
+    const { todos } = this.state
+    return (
+      <div>
+       <List name='Bucket List' items={ todos } />
+       
+      </div>
+    )
+  }
 }
+
 
 export default App;
